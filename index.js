91888d541b81ddedaf04872a0c15b7fb48b0d517
DBenson1017 <devinbenson@gmail.com>
2020-08-28 13:31:05 -0400

    js linked to html, working GET request w/ fetch

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 //COVID-19 tracker API 
+
+
+
+function loadData(){
+
+fetch('https://api.covidtracking.com/v1/us/daily.json')
+    .then(function(response){
+        return response.json()
+    })
+    .then(function(data){
+        console.log(data)
+        console.log('success')
+    })
+}
+
+loadData()
